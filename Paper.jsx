6a0b5c3e0850aa6a4269ffa7cb537880b4c612da
iGodHackerr <devildeveloper5880@icloud.com>
2025-01-31 10:57:51 +0530

    Add files via upload

---
+++ Paper.jsx
@@ -0,0 +1,29 @@
+
+import { forwardRef, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+
+export const Paper = forwardRef((props, ref) => {
+  const { nodes, materials } = useGLTF("./models/paper.glb");
+
+  const paper = useRef();
+
+  useFrame((state, delta) => {
+    paper.current.rotation.y += delta * 0.25;
+  });
+
+  return (
+    <group {...props} ref={paper} dispose={null}>
+      <mesh
+        ref={ref}
+        name="paper"
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube_Cube001.geometry}
+        material={materials["Material.000"]}
+      />
+    </group>
+  );
+});
+
+useGLTF.preload("./models/paper.glb");
---
+++ Rock.jsx
@@ -0,0 +1,31 @@
+
+import { forwardRef, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+
+export const Rock = forwardRef((props, ref) => {
+  const { nodes, materials } = useGLTF("./models/rock.glb");
+
+  const rock = useRef();
+
+  useFrame((state, delta) => {
+    rock.current.rotation.y += delta * 0.25;
+  });
+
+  return (
+    <group {...props} ref={rock} dispose={null}>
+      <mesh
+        ref={ref}
+        name="rock"
+        castShadow
+        receiveShadow
+        geometry={nodes.Rock.geometry}
+        material={materials.Stone}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={100}
+      />
+    </group>
+  );
+});
+
+useGLTF.preload("./models/rock.glb");
